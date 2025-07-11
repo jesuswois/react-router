2fddf998016b42d8561c6e33945df0628358e941
Jesús Domínguez Ramírez <jesus.dominguez.ramirez855@gmail.com>
2025-07-11 11:24:20 -0600

    Inicialización del proyecto

---
+++ react-router/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+export default function Navbar () {
+    return (
+        <div>
+            <Link to="/">Link 1
+            </Link>
+            <Link to="/">Link 2
+            </Link>
+            <Link to="/">Link 3
+            </Link>
+        </div>
+    )
+}
